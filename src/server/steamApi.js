cec9f4c90e8535fd6b0996348d301878de71f290
ye66w <popov806@gmail.com>
2025-12-19 20:14:02 +0500

    save test

--- src/server/steamApi.js
+++ src/server/steamApi.js
@@ -1,6 +1,6 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
 
-const steamApiUrl = 'https://partner.steam-api.com';
+const steamApiUrl = 'https://partner.steam-api.com'
 
 export async function checkSteamId(steamId) {
   try {
@@ -8,19 +8,21 @@ export async function checkSteamId(steamId) {
       key: process.env.STEAM_API_KEY,
       steamid: steamId,
       appid: process.env.FLOSS_APPID,
-    });
+    })
 
-    const requestUrl = `${steamApiUrl}/ISteamUser/CheckAppOwnership/v4/?${params.toString()}`;
-    const response = await fetch(requestUrl);
-    const data = await response.json();
+    const requestUrl = `${steamApiUrl}/ISteamUser/CheckAppOwnership/v4/?${params.toString()}`
+		console.log(requestUrl)
+    const response = await fetch(requestUrl)
+		console.log(response)
+    const data = await response.json()
 
     if (data && data.ownsapp) {
-      return true;
+      return true
     } else {
-      return false;
+      return false
     }
   } catch (error) {
-    console.error(error);
-    return false;
+    console.error(error)
+    return false
   }
 }
